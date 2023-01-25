010eab9c00b37e8bcc9ccad09a06598c614f8d7a
Florin Gologan <gflo13@yahoo.com>
2023-01-25 16:36:10 +0200

    Added api stack

---
+++ stacks/ApiStack.js
@@ -0,0 +1,29 @@
+import { Api, use } from '@serverless-stack/resources'
+import { StorageStack } from './StorageStack'
+
+export function ApiStack({ stack, app }) {
+
+    const { table } = use(StorageStack)
+
+    const api = new Api(stack, "Api", {
+        defaults: {
+            function: {
+                permissions: [table],
+                environment: {
+                    TABLE_NAME: table.tableName,
+                }
+            }
+        },
+        routes: {
+            "POST /notes": "functions/create.main"
+        }
+    })
+
+    stack.addOutputs({
+        ApisEndpoint: api.url
+    })
+
+    return {
+        api
+    }
+}
