8245504f5afb95ccf96eafecc06662fce0aed613
Florin Gologan <gflo13@yahoo.com>
2023-01-25 16:10:30 +0200

    Added storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,23 @@
+import { Bucket, Table } from "@serverless-stack/resources";
+
+export function StorageStack({ stack, app }) {
+    const table = new Table(stack, "Notes", {
+        fields: {
+            userId: "string",
+            noteId: "string",
+        },
+        primaryIndex: {
+            partitionKey: "userId",
+            sortKey: "noteId"
+        }
+    });
+
+    const bucket = new Bucket(stack, "Uploads", {
+        name: "sts-notes",
+    });
+
+    return {
+        table,
+        bucket,
+    }
+}
